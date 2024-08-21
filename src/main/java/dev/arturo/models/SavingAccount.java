82644c05d6fb611e9c96461d04991e42308885b0
Arturo Alvarez <153916290+ArturoAlvarezY@users.noreply.github.com>
2024-08-21 12:14:21 +0200

    making the methods calcMonthlyInterest and withdrawCash

--- src/main/java/dev/arturo/models/SavingAccount.java
+++ src/main/java/dev/arturo/models/SavingAccount.java
@@ -1,7 +1,7 @@
 package dev.arturo.models;
 
-public class SavingAccount extends BankAccount{
-    
+public class SavingAccount extends BankAccount {
+
     private boolean isActive;
 
     public SavingAccount(Float balance, Float annualRatePercentage, Float monthlyCommission, boolean isActive) {
@@ -17,6 +17,8 @@ public class SavingAccount extends BankAccount{
         this.isActive = isActive;
     }
 
+    /// METHODS TO DONT CONFUSE ME
+
     public Boolean accountStatus() {
         if (isActive == false) {
             return false;
@@ -24,14 +26,74 @@ public class SavingAccount extends BankAccount{
         return true;
     }
 
-    public Boolean comprobateFunds(BankAccount bankAccount){
+    public Boolean comprobateFunds() {
         if (!isActive) {
             return null;
         }
 
         if (getBalance() >= 10000) {
-        return this.isActive = true;           
+            return this.isActive = true;
         }
         return false;
     }
+
+    @Override
+
+    public Float consign(Float consign) {
+
+        if (!accountStatus() || !comprobateFunds() || consign <= 0) {
+            return null;
+        }
+        Float newBalance = this.getBalance() + consign;
+        this.setBalance(newBalance);
+
+        this.setConsignmentsNumbers(getConsignmentsNumbers() + 1);
+
+        return this.getBalance();
+    }
+
+    @Override
+    public Float withdrawCash(Float quantity) {
+        if (!comprobateFunds()) {
+            return null;
+        }
+
+        if (quantity <= 0) {
+            System.out.println("Quantity must be greater than 0");
+            return null;
+        }
+
+        Float actualBalance = this.getBalance() - quantity;
+
+        this.setBalance(actualBalance);
+
+        return this.getBalance();
+    }
+
+    @Override
+    public Float calcMonthlyInterest() {
+
+        Float monthlyInterest = (getAnnualRatePercentage() / 12) * getBalance();
+
+        if (this.getAnnualRatePercentage() <= 0) {
+            monthlyInterest = 0f;
+        }
+
+        Integer widthments = getConsignmentsNumbers();
+        Float balanceAfterMonthlyInterest = getBalance() - monthlyInterest;
+
+        if (widthments > 4) {
+            Float newComission = 1000f;
+            return balanceAfterMonthlyInterest - newComission;
+        }
+
+        if (balanceAfterMonthlyInterest < 10000f) {
+            System.out.println("Your account must have more than 10k to be a saving account");
+            return null;
+        }
+
+        return balanceAfterMonthlyInterest;
+
+    }
+
 }
--- src/test/java/SavingAccountTest.java
+++ src/test/java/SavingAccountTest.java
@@ -8,7 +8,7 @@ import dev.arturo.models.SavingAccount;
 
 public class SavingAccountTest {
 
-    SavingAccount savingAccount = new SavingAccount(10000f, null, null, true);
+    SavingAccount savingAccount = new SavingAccount(10000f, 1f, null, true);
 
     @Test
     void testAccountStatus() {
@@ -26,7 +26,7 @@ public class SavingAccountTest {
     void testIfBalanceIsMinnorThanTenThousandComprobateFundsIsFalse() {
         savingAccount.setBalance(9999F);
 
-        assertThat(savingAccount.comprobateFunds(savingAccount), is(false));
+        assertThat(savingAccount.comprobateFunds(), is(false));
     }
 
     @Test
@@ -35,8 +35,73 @@ public class SavingAccountTest {
 
         savingAccount.setBalance(10000000f);
 
-        assertThat(savingAccount.comprobateFunds(savingAccount), is(nullValue()));
+        assertThat(savingAccount.comprobateFunds(), is(nullValue()));
     }
 
+    @Test
+    void testConsignMethod(){
+        savingAccount.consign(1f);
+
+        assertThat(savingAccount.getBalance(), is(10001f));
+        assertThat(savingAccount.getConsignmentsNumbers(), is(1));
+    }
+
+    @Test
+    void testIfAccountStatusIsFalseReturnNull(){
+        savingAccount.setActive(false);
+
+        assertThat(savingAccount.consign(10000000f), is(nullValue()));
+    }
+    @Test
+    void testIfComprobateFoundsIsFalseReturnNull(){
+        savingAccount.setBalance(100f);
+
+        assertThat(savingAccount.consign(10000000f), is(nullValue()));
+    }
+
+    @Test
+    void testIfConsignParameterIs0ReturnNull(){
+        savingAccount.setBalance(100f);
+
+        assertThat(savingAccount.consign(0f), is(nullValue()));
+    }
+
+    @Test
+    void testWithDrawCashIsNullIfComprobateFundsIsNull(){
+        savingAccount.setBalance(100f);
+
+        assertThat(savingAccount.withdrawCash(10000000f), is(nullValue()));
+
+    }
+    
+    @Test
+    void testWithDrawCashIsNullIfParameterIsEqualOrMinorThanZero(){
+
+        assertThat(savingAccount.withdrawCash(0F), is(nullValue()));
+        assertThat(savingAccount.withdrawCash(-1F), is(nullValue()));
+
+    }
+
+    @Test
+    void testWithDrawCash(){
+        assertThat(savingAccount.withdrawCash(5000f), is(5000f));
+    }
+
+    @Test
+    void testCalcMonthlyInterestIsNullIfBalanceAfterMonthlyInterestIsMinnorThan10Thousand(){
+        assertThat(savingAccount.calcMonthlyInterest(), is(nullValue()));
+    }
+    @Test
+    void testCalcMonthlyInterestAddsOneThousandOfComisionIfConsignmentsNumbersIsGreatherThanFour(){
+        savingAccount.setConsignmentsNumbers(5);
+        savingAccount.setBalance(11000f);
+        assertThat(savingAccount.calcMonthlyInterest(), is(9083.333f));
+    }
+
+    @Test 
+    void testCalcMonthlyInterest(){
+        savingAccount.setBalance(11000f);
+        assertThat(savingAccount.calcMonthlyInterest(), is(10083.333f));
+    }
 
 }
